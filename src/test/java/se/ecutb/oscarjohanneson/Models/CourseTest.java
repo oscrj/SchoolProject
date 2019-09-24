12019e60a63a406c57c090fbdbfc771c8d36354e
oscarjohanneson <oscar.johanneson@gmail.com>
2019-09-24 15:16:50 +0200

    Added test and tested create student and course

--- src/test/java/se/ecutb/oscarjohanneson/Models/CourseTest.java
+++ src/test/java/se/ecutb/oscarjohanneson/Models/CourseTest.java
@@ -1,5 +1,47 @@
 package se.ecutb.oscarjohanneson.Models;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CourseTest {
+
+    Student testStudent;
+    Student testStudent2;
+    Course course;
+    List<Student> students;
+
+
+    @Before
+    public void setUp(){
+        testStudent = new Student(1, "anna", "anna@gmail.com", "Storgatan 1");
+        testStudent2 = new Student(2 , "Anders", "anders@gmail.com", "Parkgatan 2");
+        students = new ArrayList<>(Arrays.asList(testStudent));
+        course = new Course(1, "Matematik", LocalDate.parse("2019-08-03"), 6, students);
+    }
+
+    @Test
+    public void test_If_Course_Was_Created(){
+        Assert.assertEquals(1, course.getId());
+        Assert.assertEquals("Matematik", course.getCourseName());
+        Assert.assertEquals(LocalDate.parse("2019-08-03"), course.getStartDate());
+        Assert.assertEquals(6, course.getWeekDuration());
+        Assert.assertEquals(students, course.getStudentList());
+    }
+
+    @Test
+    public void test_If_Student_Was_Added(){
+
+    }
+
+    @Test
+    public void test_If_Student_Was_removed(){
+
+    }
 
 }
--- src/test/java/se/ecutb/oscarjohanneson/Models/StudentTest.java
+++ src/test/java/se/ecutb/oscarjohanneson/Models/StudentTest.java
@@ -1,5 +1,18 @@
 package se.ecutb.oscarjohanneson.Models;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
+
+    @Test
+    public void test_If_Student_Was_created(){
+        Student testStudent = new Student(1, "Anna", "anna@gmail.com", "Storgatan 1");
+
+        Assert.assertNotNull(testStudent.getId());
+        Assert.assertEquals("Anna", testStudent.getName());
+        Assert.assertEquals("anna@gmail.com", testStudent.getEmail());
+        Assert.assertEquals("Storgatan 1", testStudent.getAddress());
+    }
 
 }
